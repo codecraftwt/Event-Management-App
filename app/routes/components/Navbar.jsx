964703d1bb36fcb9f462d903c7403ac89a01c7a7
codecraftwt <codecraftwt@gmail.com>
2025-10-31 16:02:10 +0530

    Navbar design updated , Calender extension designed , Details page design , App url Deployments

--- app/routes/components/Navbar.jsx
+++ app/routes/components/Navbar.jsx
@@ -1,41 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
-const Navbar = ( { onCreateClick } ) => {
+const Navbar = ({ onCreateClick }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        {/* Logo */}
         <div className="navbar-logo">
           <img
-            src="https://demo.wp-eventmanager.com/wp-content/uploads/2023/03/cropped-wpem-logo.png"
+            src="/eventlogo.png"
             alt="Event Calendar Logo"
           />
         </div>
 
-        {/* Links */}
-        <ul className="navbar-links">
+        <button className={`hamburger ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu} aria-label="Toggle menu">
+          <span className="hamburger-line"></span>
+          <span className="hamburger-line"></span>
+          <span className="hamburger-line"></span>
+        </button>
+
+        <ul className={`navbar-links ${isMenuOpen ? 'open' : ''}`}>
           <li>
-            <a href="#">Home</a>
+            <a href="#" onClick={() => setIsMenuOpen(false)}>
+              <svg className="nav-icon" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10.707 2.293a1 1 0 00-1.414 0l-6 6A1 1 0 003 9v8a1 1 0 001 1h3a1 1 0 001-1v-3a1 1 0 011-1h2a1 1 0 011 1v3a1 1 0 001 1h3a1 1 0 001-1V9a1 1 0 00-.293-.707l-6-6z"/>
+              </svg>
+              Home
+            </a>
           </li>
           <li>
-            <a href="#">Events</a>
+            <a href="#" onClick={() => setIsMenuOpen(false)}>
+              <svg className="nav-icon" fill="currentColor" viewBox="0 0 20 20">
+                <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"/>
+              </svg>
+              Events
+            </a>
           </li>
           <li>
-            <a href="#">Tickets</a>
+            <a href="#" onClick={() => setIsMenuOpen(false)}>
+              <svg className="nav-icon" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z"/>
+              </svg>
+              Tickets
+            </a>
           </li>
           <li>
-            <a href="#">About</a>
+            <a href="#" onClick={() => setIsMenuOpen(false)}>
+              <svg className="nav-icon" fill="currentColor" viewBox="0 0 20 20">
+                <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"/>
+              </svg>
+              About
+            </a>
           </li>
           <li>
-            <a href="#">Contact</a>
+            <a href="#" onClick={() => setIsMenuOpen(false)}>
+              <svg className="nav-icon" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"/>
+              </svg>
+              Contact
+            </a>
           </li>
         </ul>
 
-        {/* Button */}
         <div className="navbar-button">
-          {/* <a href="/create-event" className="btn-primary">Create Event</a> */}
           <button onClick={onCreateClick} className="btn-create">
+            <svg className="btn-icon" fill="currentColor" viewBox="0 0 20 20">
+              <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd"/>
+            </svg>
             Create Event
           </button>
         </div>
